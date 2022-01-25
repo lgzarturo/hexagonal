f03a56d5b5a87d311cd462ceb469eb809d480197
Arturo López <lgzarturo@gmail.com>
2022-01-25 03:23:13 -0500

    build: Configuración del framework

---
+++ framework/config/core.php
@@ -0,0 +1,75 @@
+<?php
+
+define('IS_LOCAL', in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')));
+
+define('TIMEZONE', 'America/Cancun');
+
+define('LANGUAGE', 'es');
+
+define('WEB_URI', IS_LOCAL ? dirname($_SERVER['SCRIPT_NAME']) : '/');
+
+define('BASE_PATH', IS_LOCAL ? dirname(__FILE__, 3) : '');
+
+define('APP_SALT', 'HexagonalFramework');
+
+define('URL', IS_LOCAL ? 'http://localhost' . WEB_URI : 'https://hexagonal.local');
+
+define('DS', DIRECTORY_SEPARATOR);
+
+define('ROOT', getcwd() . DS);
+
+define('APP', ROOT . 'app' . DS);
+
+define('CONTROLLERS', APP . 'controllers' . DS);
+
+define('MODELS', APP . 'models' . DS);
+
+define('VIEWS', APP . 'views' . DS);
+
+define('FRAMEWORK', ROOT . 'framework' . DS);
+
+define('CONFIG', FRAMEWORK . 'config' . DS);
+
+define('CORE', FRAMEWORK . 'core' . DS);
+
+define('HELPERS', FRAMEWORK . 'helpers' . DS);
+
+define('PLUGINS', ROOT . 'plugins' . DS);
+
+define('TEMPLATES', ROOT . 'templates' . DS);
+
+define('LAYOUTS', TEMPLATES . 'layouts' . DS);
+
+define('SNIPPETS', TEMPLATES . 'snippets' . DS);
+
+define('RESOURCES', URL . '/resources');
+
+define('FAVICON', RESOURCES . '/favicon');
+
+define('FONTS', RESOURCES . '/fonts');
+
+define('IMAGES', RESOURCES . '/images');
+
+define('SCRIPTS', RESOURCES . '/scripts');
+
+define('STYLES', RESOURCES . '/styles');
+
+define('UPLOADS', RESOURCES . '/uploads');
+
+define('DB_ENGINE', IS_LOCAL ? 'mysql' : '');
+
+define('DB_HOST', IS_LOCAL ? 'localhost' : '');
+
+define('DB_PORT', IS_LOCAL ? '3307' : '');
+
+define('DB_NAME', IS_LOCAL ? 'hexagonal' : '');
+
+define('DB_USER', IS_LOCAL ? 'root' : '');
+
+define('DB_PASS', IS_LOCAL ? 'root' : '');
+
+define('DB_CHARSET', IS_LOCAL ? 'utf8' : '');
+
+define('DB_COLLATION', IS_LOCAL ? 'utf8_general_ci' : '');
+
+date_default_timezone_set(TIMEZONE);
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include_once('framework/config/core.php');
+
+echo "<a href=''" . URL . "''>HexagonalFramework</a>";
+
+echo "<br  />Hola Hexagonal Framework is online";
